caed3f3f5ff509c77b69b3869c61676bd4d2bcd7
Niclas Nilsson <niclas@Niclass-MacBook-Pro.local>
2015-09-21 20:39:44 +0200

    Show random product

--- app/scripts/controllers/product.js
+++ app/scripts/controllers/product.js
@@ -1,11 +1,19 @@
 "use strict";
 
-angular.module("appApp").controller("ProductCtrl", function ($scope, $http, $routeParams, ENV) {
+angular.module("appApp").controller("ProductCtrl", function ($scope, $http, $routeParams, $location, ENV) {
   var apiEndpoint = ENV.API_ENDPOINT;
 
   $http
     .get(apiEndpoint + "/products/" + $routeParams.barcode)
     .then(function (res) {
       $scope.product = res.data;
-  });
+    });
+
+  $scope.randomProduct = function () {
+    $http
+      .get(apiEndpoint + "/products/random")
+      .then(function (res) {
+        $scope.product = res.data;
+      });
+  };
 });
